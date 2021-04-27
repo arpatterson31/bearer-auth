5f1f4e4d93a54dbb209c8ecffd6021e16d587416
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-04-27 16:39:35 -0700

    retyped about half of the starter code... fixed a few bugs I spotted along the way

---
+++ src/server.js
@@ -0,0 +1,37 @@
+'use strict';
+
+// 3rd party resources
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+
+// Esoteric resources
+const errorHandler = require('./error-handlers/500.js');
+const notFound = require('./error-handlers/404.js');
+const authRoutes = require('./auth/routes.js');
+
+// Prepare the express app
+const app = express();
+
+// App Level MW
+app.use(cors());
+app.use(morgan('dev'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.use(authRoutes);
+
+// Catchalls
+app.use('*', notFound);
+app.use(errorHandler);
+
+module.exports = {
+  server: app,
+  startup: (port) => {
+    app.listen(port, () => {
+      console.log(`Server up on ${port}`);
+    });
+  }
+}
