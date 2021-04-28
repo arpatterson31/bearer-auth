138f513c3f2f57bbe2307cc5174976f781efa4a3
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-04-27 17:02:51 -0700

    retyped the user model from starter code, found a lot of bugs in that file

---
+++ src/auth/models/user.js
@@ -0,0 +1,46 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken'); // bug need to bring in jwt
+
+const users = new mongoose.Schema({
+  username: { type: String, require: true, unique: true },
+  password: { type: String, required: true }
+}, { toJSON: { virtuals: true }}); // bug need 2nd arguement for .virtual
+
+// Add a virtual to schema and get a token on every user object
+users.virtual('token').get(function () {
+  let tokenObject = {
+    username: this.username
+  }
+  return jwt.sign(tokenObject, process.env.APP_SECRET); // bug needs APP secret as 2nd param
+});
+
+users.pre('save', async function () {
+  if(this.isModified('password')) {
+    this.password = bcrypt.hash(this.password, 10);
+  }
+});
+
+// Basic Auth
+users.statics.authenticateBasic = async function (username, password) {
+  const user = await this.findOne({ username });
+  const valid = await bcrypt.compare(password, user.password);
+  if (valid) { return user; }
+  throw new Error('Invalid User');
+}
+
+// Bearer Auth
+users.statics.authenticateWithToken = async function (token) {
+  try {
+    const parsedToken = jwt.verify(token, process.env.APP_SECRET);
+    const user = this.findOne({ username: parsedToken.username });
+    if (user) { return user; }
+    throw new Error('User Not Found');
+  } catch (e) {
+    throw new Error(e.message);
+  }
+}
+
+module.exports = mongoose.model('users', users);
